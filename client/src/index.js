cb2e52ebebb69b79e7e33d2b3885aff66b41f857
Josh Jarmain <jjarmain@gmail.com>
2018-03-11 07:29:05 -0700

    moving from boost to use subscriptions

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,36 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { split } from 'apollo-link'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
 
-// Pass your GraphQL endpoint to uri
-const client = new ApolloClient({ uri: 'http://localhost:4006/graphql' });
+const httpLink = createHttpLink({ uri: 'http://localhost:4006/graphql'  })
+
+const wsLink = new WebSocketLink({
+  uri: 'ws://localhost:4006/subscriptions',
+  options: {
+    reconnect: true
+  }
+})
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  httpLink,
+)
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache().restore(window.__APOLLO_STATE__),
+});
 
 ReactDOM.render(
     <ApolloProvider client={client}
